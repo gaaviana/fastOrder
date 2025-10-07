c3e4e3f26b89ec697a5f8176ede358a348d03410
Gabriel Viana <gaa.vianaa@gmail.com>
2025-10-06 22:52:56 -0300

    função enviar pedido

---
+++ src/lib/pedidos.js
@@ -0,0 +1,18 @@
+import { supabase } from "./supabase";
+
+export async function enviarPedidos(itensPendentes, mesaId) {
+  if (itensPendentes.length === 0) return;
+
+  const pedidos = itensPendentes.map((item) => ({
+    mesa_id: String(mesaId),
+    item_nome: item.nome,
+    preco: item.preco,
+    status: "pendente"
+  }))
+
+  const {error} = await supabase.from("pedidos").insert(pedidos)
+
+  if (error) {
+    throw new Error("Não foi possivel enviar os pedidos. tente novamente")
+  }
+}
